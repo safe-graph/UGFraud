794566b4e7362ecb7860534a7b30eb474002ac47
Yingtong Dou <ytongdou@gmail.com>
2020-06-17 18:53:28 -0500

    update files

--- Detector/eval_fBox.py
+++ Detector/eval_fBox.py
@@ -1,6 +1,9 @@
 
 from math import *
 import numpy as np
+import pickle as pkl
+import sys
+sys.path.insert(0, sys.path[0] + '/..')
 
 from Utils.iohelper import *
 from Utils.yelpFeatureExtraction import *
@@ -62,26 +65,16 @@ def runfBox(new_priors, user_product_graph):
 if __name__ == '__main__':
 
 	dataset_name = 'YelpChi'
-	prefix = 'Yelp_Dataset/' + dataset_name + '/'
+	prefix = '../Yelp_Dataset/' + dataset_name + '/'
 	metadata_filename = prefix + 'metadata.gz'
 
 	# load graph and label
 	user_product_graph, product_user_graph = read_graph_data(metadata_filename)
 	user_ground_truth, review_ground_truth = create_ground_truth(user_product_graph)
 
-	# feature and prior calculation
-	feature_suspicious_filename = 'Utils/feature_configuration.txt'
-	review_feature_list = ['RD', 'EXT', 'EXT', 'DEV', 'ETF', 'ISR']
-	user_feature_list = ['MNR', 'PR', 'NR', 'avgRD', 'BST', 'ERD', 'ETG']
-	product_feature_list = ['MNR', 'PR', 'NR', 'avgRD', 'ERD', 'ETG']
-	feature_config = load_feature_config(feature_suspicious_filename)
-	feature_extractor = FeatureExtractor()
-	UserFeatures, ProdFeatures, ReviewFeatures = feature_extractor.construct_all_features(user_product_graph,
-																						  product_user_graph)
-	upriors = feature_extractor.calculateNodePriors(user_feature_list, UserFeatures, feature_config)
-	ppriors = feature_extractor.calculateNodePriors(product_feature_list, ProdFeatures, feature_config)
-	rpriors = feature_extractor.calculateNodePriors(review_feature_list, ReviewFeatures, feature_config)
-	priors = [upriors, rpriors, ppriors]
+	# load priors
+	with open(prefix + 'priors.pkl', 'rb') as f:
+		priors = pkl.load(f)
 
 	userBelief, reviewBelief = runfBox(priors, user_product_graph)
 	reviewBelief = scale_value(reviewBelief)
